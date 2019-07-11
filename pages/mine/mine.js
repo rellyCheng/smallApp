8f71a7dcf7c7adfc56310e2e1384fdad882788e8
rellyCheng <707471634@qq.com>
2019-07-11 13:59:04 +0800

    我的页面

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,36 +1,71 @@
 // pages/mine/mine.js
+//获取应用实例
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-   
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          console.log(res)
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
-  canvasIdErrorCallback: function (e) {
-    console.error(e.detail.errMsg)
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    //使用wx.createContext获取绘图上下文context
-    var ctx = wx.createCanvasContext('firstCanvas')
-    ctx.moveTo(10, 10)
-    ctx.lineTo(100, 20)
-    ctx.lineTo(100, 100)
-    ctx.closePath()
-    ctx.setFillStyle('red')
-    ctx.fill()
-    ctx.draw()
-  },
-
-
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    wx.login({
+      success(res) {
+        console.log(res)
+        if (res.code) {
+          //发起网络请求
+          // wx.request({
+          //   url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wxa0b3f7973fef558c&secret=556a36dd25cd4e01a131946e6f5e7fac&js_code=' + res.code+'&grant_type=authorization_code',
+          //   success(res) {
+          //     console.log(res)
+          //   }
+          // })
+          
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  }
 })
