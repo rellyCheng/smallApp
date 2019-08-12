e9b0d8bdd1662697cfb027447e65ca737b360716
rellyCheng <707471634@qq.com>
2019-08-12 22:17:35 +0800

    字体
    使用在线字体

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -3,12 +3,10 @@
 const app = getApp();
 const wxRequest = require('./../../utils/wxRequest.js');
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -22,8 +20,6 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         console.log(res)
         this.setData({
@@ -31,19 +27,7 @@ Page({
           hasUserInfo: true
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      // wx.getUserInfo({
-      //   success: res => {
-      //     console.log(res)
-      //     app.globalData.userInfo = res.userInfo
-      //     this.setData({
-      //       userInfo: res.userInfo,
-      //       hasUserInfo: true
-      //     })
-      //   }
-      // })
-    }
+    } 
   },
   getUserInfo: function (e) {
     console.log(e)
